168eff11f7dd0531a44de7b6cc86b348bff9b1b9
slavo <sswietlik@gmail.com>
2020-06-05 00:35:48 +0200

    Formatowanie napisów 1 Lab unfinished

---
+++ formatowanieNapisowLab_1.py
@@ -0,0 +1,14 @@
+helloMessage1 = "Hello %s!"
+print(helloMessage1 %'Kate')
+print(helloMessage1 %'Chris')
+
+print()
+helloMessage2= "Hello {0:s}"
+print(helloMessage2.format('Chris'))
+print(helloMessage2.format('Kate'))
+print()
+helloMessage3 = "%s has %d %s" #(%s has, 1 ,
+print('komunikat 1')
+print(helloMessage3 % ('Kate',1,'animal'))
+
+
