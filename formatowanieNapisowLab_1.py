ef645d3ab32d5dc9beb946db8037b09d8369c2a2
slavo <sswietlik@gmail.com>
2020-06-05 09:54:51 +0200

    Formatowanie napisów pt1LAB

--- formatowanieNapisowLab_1.py
+++ formatowanieNapisowLab_1.py
@@ -7,8 +7,23 @@ helloMessage2= "Hello {0:s}"
 print(helloMessage2.format('Chris'))
 print(helloMessage2.format('Kate'))
 print()
-helloMessage3 = "%s has %d %s" #(%s has, 1 ,
+helloMessage3 = "%5s has %7d %7s"
 print('komunikat 1')
 print(helloMessage3 % ('Kate',1,'animal'))
-
-
+print(helloMessage3 % ('Chris',1000,'$'))
+print()
+print("Komunikat 1 - użycie FORMAT")
+helloMessage3 = "{0:s} has {1:d} {2:s}"
+print(helloMessage3.format('Kate',1,'animal'))
+print(helloMessage3.format('Chris',1000,'$'))
+print()
+line="%20s cost %6d EUR"
+print(line %('ICE CREAM',3))
+print(line %('TRIP TO VENICE',119))
+print(line %('PIZZA HAWAI',6))
+print()
+print('wersja 2')
+line2='{0:20s} cos {1:6d} EUR'
+print(line2.format('ICE CREAM',3))
+print(line2.format('TRIP TO VENICE',119))
+print(line2.format('PIZZA HAWAI',6))
