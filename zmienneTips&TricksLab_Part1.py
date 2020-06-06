0822e237eb3692723a7470fdbee2c3169ee2fb36
slavo <sswietlik@gmail.com>
2020-06-06 09:08:59 +0200

    Zmienne T&T

---
+++ zmienneTips&TricksLab_Part1.py
@@ -0,0 +1,25 @@
+v1=126
+v2='126'
+v3=126.0
+v4='126.0'
+print()
+print(type(v1))
+print(type(v2))
+print(type(v3))
+print(type(v4))
+
+print(v1+v3)
+print(v2+v4)
+
+print(7-1*0+3+3)
+
+print(4**5/2**3)
+
+print()
+# 9 naciskam max 4razy
+# dziła przycisk + - * / ()
+# napisać działanie tak aby wynik wyszedł 100
+
+print(9/9)
+print(9+9)
+print((99+9/9))
---
+++ zmienneTips&Tricks_1.py
@@ -0,0 +1,50 @@
+
+print()
+Title ='Python'
+print('Title is',type(Title))
+print()
+
+Version = 3
+print('Version is',type(Version))
+print()
+
+Progress = 0.21
+print('Progress is',type(Progress))
+print()
+
+print('This expression is', type(Progress * Version))
+print()
+
+x=1
+y=1
+z=1
+print(x,y,z)
+
+a=b=c=2
+print(a,b,c)
+
+c=3
+print(a,b,c)
+print()
+
+print(2+2*2)
+
+print(6/2*(1+2))
+print()
+
+print(4**3**2) # podwójna gwiazdka oznacza potęgowanie 2**2 = 4 (w tym przypadku 4^3^2 = 4^9)
+print()
+
+x=1
+print(x+1)
+print(x)
+
+x=x+1
+print(x)
+
+x+=1
+print(x)
+
+x-=1
+print(x)
+
