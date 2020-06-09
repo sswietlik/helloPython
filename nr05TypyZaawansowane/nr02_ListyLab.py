b64b0b5556962a70f68fba1f2d38c98e79dc8faf
slavo <sswietlik@gmail.com>
2020-06-09 09:30:04 +0200

    Typy Zaawansowane->ListyLab

---
+++ nr05TypyZaawansowane/nr02_ListyLab.py
@@ -0,0 +1,9 @@
+hitsTitles=['BROTHERS IN ARMS','BOHEMIAN RHAPSODY','STAIRWAY TO HEAVEN','RIDERS ON THE STORM','WISH YOU WERE HERE']
+print(hitsTitles)
+
+hitsTitles.append(('CHILD IN TIME'))
+hitsTitles.append(('AGAIN'))
+print(hitsTitles)
+
+hitsTitles.insert(3,'HOTEL CALIFORNIA')
+
