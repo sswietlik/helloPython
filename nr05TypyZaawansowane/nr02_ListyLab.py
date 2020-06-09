7aced22f96c6ebec65860ee6c4afb5358369322f
slavo <sswietlik@gmail.com>
2020-06-09 10:00:51 +0200

    ListyLab

--- nr05TypyZaawansowane/nr02_ListyLab.py
+++ nr05TypyZaawansowane/nr02_ListyLab.py
@@ -5,5 +5,43 @@ hitsTitles.append(('CHILD IN TIME'))
 hitsTitles.append(('AGAIN'))
 print(hitsTitles)
 
-hitsTitles.insert(3,'HOTEL CALIFORNIA')
+hitsTitles.insert(2,'HOTEL CALIFORNIA')
+print(hitsTitles)
+
+hitsTitles.insert(0,'THE SOUND OF SILENCE')
+print(hitsTitles)
+
+print(hitsTitles.index('HOTEL CALIFORNIA'))
+
+hitsTitles.remove(('HOTEL CALIFORNIA'))
+print(hitsTitles)
+
+hitsTitles.remove(('THE SOUND OF SILENCE'))
+hitsTitles.insert(0,'ENJOY THE SILENCE')
+print(hitsTitles)
+print()
+print('kopia edytowalna listy glownej')
+hitsToRead = hitsTitles.copy()
+print(hitsToRead)
+
+hitsToRead.reverse()
+print(hitsToRead)
+
+hitsToRead.sort()
+print(hitsToRead)
+
+print(hitsToRead.pop(0),",",hitsToRead.pop(0))
+print(hitsToRead)
+
+additionalSongs=['NOTHING COMPARES 2 U','WISH YOU WERE HERE']
+print(additionalSongs)
+hitsToRead.extend(additionalSongs)
+print(hitsToRead)
+
+print(hitsToRead.count('WISH YOU WERE HERE'))
+print(hitsToRead.count('RIDERS ON THE STORM'))
+
+hitsToRead.clear()
+print(hitsToRead)
+
 
