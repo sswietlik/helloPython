9ddae609b09fe02eca87f21032c6f2cf8fd4a594
slavo <sswietlik@gmail.com>
2020-06-20 10:52:32 +0200

    petle

---
+++ nr07_Petle/nr01_While.py
@@ -0,0 +1,18 @@
+i=1
+imax=10
+
+while i <= imax:
+    print(i,'I like Python')
+    i+=1 #inkrementacja - wzrost wartości
+else:
+    print('Now i >= 10 which is equal =',i)
+print()
+
+i=10
+imax=0
+
+while i >= imax:
+    print(i,'I like Python')
+    i-=1 #inkrementacja - wzrost wartości
+else:
+    print('Now i =' ,i+1)
---
+++ nr07_Petle/nr02_While_LAB.py
@@ -0,0 +1,24 @@
+print('Zad 1')
+print()
+
+firstRow=1
+lastRow=31
+currentRow=firstRow
+
+while currentRow <= lastRow:
+    print('Row number', currentRow)
+    currentRow+=1
+print()
+
+print('Zad 2')
+print()
+
+numberMin = 1
+numbberMax = 13
+numberActual = numberMin
+
+while numberActual <= numbberMax:
+    print(numberActual, '^2=', numberActual**2,)
+    print(numberActual, '^3=', numberActual**3,)
+    numberActual+=1
+    print()
