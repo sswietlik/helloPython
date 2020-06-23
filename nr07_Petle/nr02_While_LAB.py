7d63c327b05e7bad8d297be5edb26cf9e5649e6a
slavo <sswietlik@gmail.com>
2020-06-23 23:52:29 +0200

    whileLab

--- nr07_Petle/nr02_While_LAB.py
+++ nr07_Petle/nr02_While_LAB.py
@@ -22,3 +22,27 @@ while numberActual <= numbberMax:
     print(numberActual, '^3=', numberActual**3,)
     numberActual+=1
     print()
+
+print('Zad 3')
+print()
+numMin = 0
+numMax = 16
+
+x = numMin
+while x <= numMax:
+    print(x, 2**x)
+    x+=1
+print()
+
+print('Zad 4')
+print()
+start=1
+end=10
+
+value = start
+while value <=end:
+    print(value,value*'x')
+    value+=1
+
+
+
