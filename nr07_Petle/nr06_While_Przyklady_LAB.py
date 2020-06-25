807d2740612d648689505f61bd6e8ec4341bf119
slavo <sswietlik@gmail.com>
2020-06-25 10:23:04 +0200

    problem z kodem w gierce , nie działa zarówno moja wersja jak i kopia kodu ze szkolenia

--- nr07_Petle/nr06_While_Przyklady_LAB.py
+++ nr07_Petle/nr06_While_Przyklady_LAB.py
@@ -2,4 +2,33 @@ print('Zad 1')
 print()
 
 number = 1
-min()
+previousNumber = 0
+while number <= 50:
+    print(number,'+', previousNumber,'=',number + previousNumber)
+    previousNumber=number
+    number+=1
+print()
+
+# -------------------------------------------------------------------
+print('Zad 2')
+print()
+
+import random
+my_number = random.randint(0,20)
+guess = -1
+trials = 0
+
+print('Guess my number!')
+
+while guess != my_number:
+    guess=int(input())
+    trials+=1
+    if guess == my_number:
+        print('Gratulacje', my_number)
+    elif guess > my_number:
+        print('Sorry my number is smaller than your guess, Try again!',guess)
+    else:
+        print('Sorry- my number is greater than your guess, Try again!',guess)
+
+
+
