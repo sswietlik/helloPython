5ca9133ec6621281c4f933d176573fcc57df604e
slavo <sswietlik@gmail.com>
2020-06-29 11:07:48 +0200

    .....

--- nr07_Petle/nr06_While_Przyklady_LAB.py
+++ nr07_Petle/nr06_While_Przyklady_LAB.py
@@ -11,24 +11,39 @@ print()
 
 # -------------------------------------------------------------------
 print('Zad 2')
+print('Wystąpil problem z kodem do zweryfikowania ')
+#
+# import random
+# my_number = random.randint(0,20)
+# guess = -1
+# trials = 0
+#
+# print('Guess my number!')
+#
+# while guess != my_number:
+#     guess=int(input())
+#     trials+=1
+#     if guess == my_number:
+#         print('Gratulacje', my_number)
+#     elif guess > my_number:
+#         print('Sorry my number is smaller than your guess, Try again!',guess)
+#     else:
+#         print('Sorry- my number is greater than your guess, Try again!',guess)
+# -------------------------------------------------------------------
+print('Zad 3')
 print()
 
-import random
-my_number = random.randint(0,20)
-guess = -1
-trials = 0
+texts = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+i = 0
 
-print('Guess my number!')
-
-while guess != my_number:
-    guess=int(input())
-    trials+=1
-    if guess == my_number:
-        print('Gratulacje', my_number)
-    elif guess > my_number:
-        print('Sorry my number is smaller than your guess, Try again!',guess)
-    else:
-        print('Sorry- my number is greater than your guess, Try again!',guess)
+max = len(texts)-1
 
 
+while i < max:
+    print(i, texts[i],texts[i+1])
+
+if len(texts[i]) == len(texts[i+1]):
+    print("\tFOUND")
+
+i+=1
 
