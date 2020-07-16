5aff8e4762b1624b47c3817bf7cb8613852f724f
slavo <sswietlik@gmail.com>
2020-07-16 14:25:53 +0200

    generator LOTTO chybił trafił wersja bez dublowania

--- nr08_Informacje_zaawansowane/nr017_random_LOTTO.py
+++ nr08_Informacje_zaawansowane/nr017_random_LOTTO.py
@@ -5,6 +5,9 @@ myNumbers = []
 while len(myNumbers) < 7:
 
     newNumber = random.randint(1,49)
-
+    # bez dublowania
+    if newNumber in myNumbers:
+        print('doubled number is :',newNumber, ', next draw')
+        continue
     myNumbers.append(newNumber)
     print(myNumbers)
