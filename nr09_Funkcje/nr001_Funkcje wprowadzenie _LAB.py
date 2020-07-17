47c440e5c615c9af482265e5eb177d1d5d984e9b
slavo <sswietlik@gmail.com>
2020-07-17 02:25:17 +0200

    Obrazki

---
+++ nr09_Funkcje/nr001_Funkcje wprowadzenie _LAB.py
@@ -0,0 +1,38 @@
+
+def PrintCat():
+#wyświetla kota
+
+    txt = r'''
+    |\---/|
+    | o_o |
+    \_^_/'''
+    print(txt)
+    return
+
+def PrintBear():
+
+    txt = r'''
+    /  \.-"""-./  \
+    \    -   -    /
+     |   o   o   |
+     \  .-'"'-.  /
+      '-\__Y__/-'
+         `---`'''
+    print(txt)
+    return
+
+def PrintBat():
+
+    txt = r'''
+      /\                 /\
+     / \'._   (\_/)   _.'/ \
+    /_.''._'--('.')--'_.''._\
+    | \_ / `;=/ " \=;` \ _/ |
+     \/ `\__|`\___/`|__/`  \/
+             \(/|\)/       '''
+    print(txt)
+    return
+
+PrintBat()
+PrintBear()
+PrintCat()
