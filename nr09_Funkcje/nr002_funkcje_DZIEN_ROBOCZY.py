4f17b3241d8d6987763d9972a41e01792bd4c19f
slavo <sswietlik@gmail.com>
2020-07-18 00:11:30 +0200

    Najbliższy dzień roboczy

---
+++ nr09_Funkcje/nr002_funkcje_DZIEN_ROBOCZY.py
@@ -0,0 +1,16 @@
+def GivenWorkingDay():
+    from datetime import date
+    from datetime import timedelta
+
+    day = date.today()
+    # day = date(2020,7,18)
+    if day.weekday() == 5:
+        workingday = day + timedelta(days=2)
+    elif day.weekday() == 6:
+        workingday = day + timedelta(days=1)
+    else:
+        workingday = day
+    print(workingday)
+    return
+
+GivenWorkingDay()
--- nr999_Testowe/nr01_zakladka_testowa.py
+++ nr999_Testowe/nr01_zakladka_testowa.py
@@ -1,65 +1,14 @@
-colors = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-figures = [
-    {'Figure': 'Ace', 'Power': 14},
-    {'Figure': 'King', 'Power': 13},
-    {'Figure': 'Queen', 'Power': 12},
-    {'Figure': 'Jack', 'Power': 11},
-    {'Figure': '10', 'Power': 10},
-    {'Figure': '9', 'Power': 9},
-    {'Figure': '8', 'Power': 8},
-    {'Figure': '7', 'Power': 7},
-    {'Figure': '6', 'Power': 6},
-    {'Figure': '5', 'Power': 5},
-    {'Figure': '4', 'Power': 4},
-    {'Figure': '3', 'Power': 3},
-    {'Figure': '2', 'Power': 2},
-    {'Figure': '1', 'Power': 1}]
+from datetime import date
+from datetime import timedelta
 
-allCards = []
-for c in colors:
-    for f in figures:
-        aCard = f.copy()
-        aCard['Color'] = c
-        allCards.append(aCard)
 
-import random
 
-random.shuffle(allCards)
-print(allCards)
-
-player1 = []
-player2 = []
-
-player1 = allCards[:12]
-player2 = allCards[12:]
-
-print('-------PLAYER 1--------')
-print(player1)
-
-print('-------PLAYER 1--------')
-print(player2)
-
-while len(player1) > 0 and len(player2) > 0:
-    card1 = player1.pop(0)
-    card2 = player2.pop(0)
-
-    if card1["Power"] == card2["Power"]:
-        player1.append(card1)
-        player2.append(card2)
-        # print('=EQUAL \t %d \t %d \t %d \t %d' % (card1["Power"], card2["Power"], len(player1), len(player2)))
-        print('=EQUAL \t %d \t %d \t' % (card1["Power"], card2["Power"]) + len(player1) * '*')
-    elif card1["Power"] > card2["Power"]:
-        player1.append(card1)
-        player1.append(card2)
-        # print('PLAY-1 \t %d \t %d \t %d \t %d' % (card1["Power"], card2["Power"], len(player1), len(player2)))
-        print('PLAY-1 \t %d \t %d \t' % (card1["Power"], card2["Power"]) + len(player1) * '*')
-    else:
-        player2.append(card2)
-        player2.append(card1)
-        # print('PLAY-2 \t %d \t %d \t %d \t %d' % (card1["Power"], card2["Power"], len(player1), len(player2)))
-        print('PLAY-2 \t %d \t %d \t' % (card1["Power"], card2["Power"]) + len(player1) * '*')
-
-if len(player1) > 0:
-    print('PLAYER 1 WON!!!!')
+day = date.today()
+# day = date(2020,7,18)
+if day.weekday() == 5:
+    workingday = day + timedelta(days=2)
+elif day.weekday() == 6:
+    workingday = day + timedelta(days=1)
 else:
-    print('PLAYER 2 WON!!!!')
+    workingday = day
+print(workingday)
